ca7f64d8af9d37ee860f7b9b342a4cbb0092bc58
astavares <astavares4@gmail.com>
2016-11-06 00:23:00 +0000

    code cleaning

--- bashflix.sh
+++ bashflix.sh
@@ -4,44 +4,31 @@ lang1="pt"
 lang2="en"
 
 get-subs () {
-  subliminal download -l $1 ${name// /.} 
+  subliminal download -l $1 ${name// /.}
 }
 
-# get magnet 
+# get magnet name
 pirate-get -s SeedersDsc -0 -M $1
-
-#pirate-get -s SeedersDsc -0 -C "echo \"%s\"" $1 |
-#  while IFS= read -r line
-#  do
-#    echo "$line" > magnet.txt
-#  done
-
-#value=$(<magnet.txt)
-#value2=$(awk -F "&" '{print $2}' <<< "$value")
-#value3=$(awk -F "=" '{print $2}' <<< "$value2")
-#value4=$(echo "$value3" | sed -e 's/%\([0-9A-F][0-9A-F]\)/\\\\\x\1/g' | xargs echo -e)
-
 magnet=$(find . -maxdepth 1 -name "*.magnet" | head -1)
 name=${magnet:2:-7}
-#name=${value4}
 
 # get subtitles
-get-subs ${lang1}
-
+get-subs $lang1
 sub=$(find . -maxdepth 1 -name "*.srt" | head -1)
 
 # try to search in english if portuguese not found
 if [ -z "$sub" ]; then
-  get-subs ${lang2}
+  get-subs $lang2
   sub=$(find . -maxdepth 1 -name "*.srt" | head -1)
 fi
 
+# remove parenthesis from subtitles filename
 sub2=$(echo "$sub" | sed -re 's/[()]//g')
 sub3=${sub2:2}
 mv ${sub:2} ${sub3}
 
-# get magnet again to peerflix stream
+# get magnet to stream
 pirate-get -s SeedersDsc -0 -C "peerflix \"%s\" -k -t ${sub3}" $1
 
 # remove created files
-rm *.srt && rm *.magnet && rm magnet.txt
+rm *.srt && rm *.magnet
