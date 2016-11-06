1e69f8c7ed454c5156e11db50613ad8228ccefb9
astavares <astavares4@gmail.com>
2016-11-06 20:16:58 +0000

    bashflix.sh now globally executable

--- install-Linux.sh
+++ install-Linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- 
+
 echo "$1" | sudo -S apt update
 echo "$1" | sudo -S apt install python3-pip -y
 echo "$1" | sudo -S apt install libxslt1-dev -y
@@ -15,4 +15,4 @@ echo "$1" | sudo -S pip install subliminal
 echo "$1" | sudo -S add-apt-repository ppa:mc3man/mpv-tests -y
 echo "$1" | sudo -S apt update && sudo apt install mpv -y
 chmod u+x bashflix.sh
-
+echo "$1" | sudo -S cp bashflix.sh /usr/local/bin
