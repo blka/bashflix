048ec18fa8ff31195e21b3193f865ba2470522f7
astavares <astavares4@gmail.com>
2016-11-07 01:23:56 +0000

    working install script. temporary tough

--- install.sh
+++ install.sh
@@ -24,66 +24,20 @@ run-with-sudo() {
   echo "${password}" | sudo -S ${@}
 }
 
-echo "Checking OS ..."
-if [[ "$OSTYPE" != "linux-gnu" ]] && [[ "$OSTYPE" != "darwin"* ]]; then
-  echo "Only Mac OS and Ubuntu are supported at the moment."
-  exit 1
-fi
-
-echo "Checking bashflix requirements ..."
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  echo "Looking for Homebrew ..."
-  if ! which brew &>/dev/null; then
-    echo "Preparing to install Homebrew ..."
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    brew install git
-  fi
-else 
-  if [[ "$OSTYPE" != "linux-gnu" ]]; then
-    run-with-sudo apt-get install -y git
-  fi
-fi
-
-echo "Looking for NPM ..."
-if ! which npm &>/dev/null; then
-  if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    run-with-sudo apt-get -y update
-    run-with-sudo apt-get install npm -y
-    curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
-    run-with-sudo apt-get install nodejs -y
-  elif [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install npm
-  fi
-fi
-
-echo "Looking for PIP3 ..."
-if ! which python3 &>/dev/null; then
-  echo "Preparing to install PIP3 ..."
-  if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    run-with-sudo apt-get -y update
-    run-with-sudo apt-get install -y python3 python3-pip
-  elif [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install python3
-  fi
-fi
-
-echo "Preparing to install MPV ..."
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  run-with-sudo add-apt-repository -y ppa:mc3man/mpv-tests
-  run-with-sudo apt-get -y update
-  run-with-sudo apt-get install -y libxslt1-dev libxml2-dev
-  run-with-sudo apt-get install -y mpv
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  brew update
-  brew install mpv
-fi
-
-run-with-sudo python3 -m pip install --no-cache-dir -I -U --upgrade pip
-run-with-sudo python3 -m pip install --upgrade pirate-get
-run-with-sudo python3 -m pip install --upgrade subliminal
-
+run-with-sudo apt update
+run-with-sudo apt install python3-pip -y
+run-with-sudo apt install libxslt1-dev -y
+run-with-sudo apt install libxml2-dev -y
+run-with-sudo pip3 install pirate-get
+run-with-sudo apt install npm -y
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+run-with-sudo apt install nodejs -y
 run-with-sudo npm install -g peerflix
+run-with-sudo apt install python-pip -y
+run-with-sudo pip install --upgrade pip
+run-with-sudo pip install subliminal
+run-with-sudo add-apt-repository ppa:mc3man/mpv-tests -y
+run-with-sudo apt update && sudo apt install mpv -y
 
 chmod u+x bashflix.sh
 
