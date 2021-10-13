147f65e275dc808b78b5ea75184db780c767da15
André Santos Tavares <andre.tavares@bitsighttech.com>
2021-10-13 03:03:58 +0100

    fixed bug

--- bashflix.sh
+++ bashflix.sh
@@ -30,7 +30,7 @@ case $1 in
     ;;
 
   "update")
-    $(curl https://raw.githubusercontent.com/0zz4r/bashflix/master/install.sh | sudo bash -)
+    $(sudo bash -c "$(curl -s https://raw.githubusercontent.com/0zz4r/bashflix/master/install.sh))
     echo "Updated!"
     exit 0
     ;;
